0e3aed26af737348a84a4d6a36f82f64a60a1488
RodrigoLago <rodrigazo98@gmail.com>
2023-08-25 13:51:59 -0300

    Agrego boton de trabaja con nosotros y corrijo el responsive

--- src/Components/TopPage/TopPage.tsx
+++ src/Components/TopPage/TopPage.tsx
@@ -1,16 +1,15 @@
 import "./TopPage.css";
-
+import WorkButton from "../WorkButton/WorkButton";
 const TopPage = () => {
     return (
         <>
 
             <div id="div-top">
-                <div id="logo" className="padding-top">
+                <div id="logo" >
                     <img src="./Resources/images/logo.png" alt="Alignment Websites" />
                 </div>
-                <p className="padding-top"> OBTEN UN SITIO WEB PROFESIONAL Y POTENCIA TU NEGOCIO</p>
-                <button > BOTON</button>
-
+                <p id="header-text"> OBTEN UN SITIO WEB PROFESIONAL Y POTENCIA TU NEGOCIO</p>
+                <WorkButton text={'Trabaja con nosotros'} />
 
             </div>
 
