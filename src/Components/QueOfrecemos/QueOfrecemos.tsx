e803dd0b221004933a88148dfe6af86a0dd32755
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-09-09 23:31:54 +0200

    icons
    se cargan iconos

--- src/Components/QueOfrecemos/QueOfrecemos.tsx
+++ src/Components/QueOfrecemos/QueOfrecemos.tsx
@@ -1,9 +1,18 @@
 import CardOffer from "./CardOffer/CardOffer";
 import "./QueOfrecemos.css";
 import mobile from "../../Resources/images/mobile.svg";
-import rapida from "../../Resources/images/rapidSi.svg";
-import rapidaSi from "../../Resources/images/rapidosesenta.svg";
-import network from "../../Resources/images/network_check_FILL0_wght400_GRAD0_opsz24 1.svg";
+import blueCarga from "../../Resources/images/blueCarga.svg";
+import blueSeo from "../../Resources/images/blueSeo.svg";
+import blueSocial from "../../Resources/images/blueSocial.svg";
+import blueSearch from "../../Resources/images/blueSearch.svg";
+import blueSupport from "../../Resources/images/blueSupport.svg";
+
+import whiteMobile from "../../Resources/images/whiteMobile.svg";
+import whiteCarga from "../../Resources/images/whiteRapida.svg";
+import whiteSeo from "../../Resources/images/whitePersonOk.svg";
+import whiteSocial from "../../Resources/images/whiteSocial.svg";
+import whiteSearch from "../../Resources/images/whiteSearch.svg";
+import whiteSupport from "../../Resources/images/whiteSupport.svg";
 
 const QueOfrecemos = () => {
   return (
@@ -21,26 +30,28 @@ const QueOfrecemos = () => {
         />
 
         <CardOffer
-          imgO={rapida}
+          imgO={blueSeo}
           titleO="Experiencia de Usuario Optimizada:"
           descriptionO="Creamos sitios web que no solo se ven bien, sino que también brindan una experiencia de usuario excepcional. La navegación fluida y la disposición intuitiva mantendrán a tus visitantes comprometidos."
         />
         <CardOffer
+          imgO={blueCarga}
           titleO="Carga Rápida y Eficiente:"
           descriptionO="Con nuestra tecnología optimizada, garantizamos que tu sitio web cargará de manera rápida y eficiente, manteniendo a tus visitantes involucrados desde el primer momento."
         />
 
         <CardOffer
+          imgO={blueSearch}
           titleO="SEO Incorporado:"
           descriptionO="La optimización para motores de búsqueda es crucial para ser encontrado en línea. Integramos técnicas de SEO en cada página que creamos, lo que aumentará tus posibilidades de aparecer en los resultados de búsqueda."
         />
         <CardOffer
-          imgO={network}
+          imgO={blueSocial}
           titleO="Redes Sociales:"
           descriptionO="Conecta tu presencia en línea de manera más efectiva mediante la integración perfecta de tus perfiles de redes sociales en tu sitio web. Esto te permite llegar a un público más amplio y mantener a tus visitantes actualizados."
         />
         <CardOffer
-          imgO={rapidaSi}
+          imgO={blueSupport}
           titleO="Soporte y Mantenimiento:"
           descriptionO="No te dejamos solo, ofrecemos mantenimiento y soporte constante para asegurarnos de que tu web siga funcionando sin problemas y esté al día con las últimas tendencias."
         />
