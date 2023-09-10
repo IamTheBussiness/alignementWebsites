67b494ec171a196d40c7a12af863b23b0c4adebe
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-09-10 10:55:40 +0200

    finish?
    terminando la rama que ofrecemos

--- src/Components/QueOfrecemos/CardOffer/CardOffer.tsx
+++ src/Components/QueOfrecemos/CardOffer/CardOffer.tsx
@@ -1,17 +1,43 @@
 import "./CardOffer.css";
+import { useState } from "react";
 
 interface CardOffersProps {
-  imgO: string;
+  imgOblue: string;
+  imgOwhite: string;
   titleO: string;
   descriptionO: string;
 }
 
-const CardOffer = ({ imgO, titleO, descriptionO }: CardOffersProps) => {
+const CardOffer = ({
+  imgOblue,
+  imgOwhite,
+  titleO,
+  descriptionO,
+}: CardOffersProps) => {
+  // eslint-disable-next-line
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <div>
-      <div className="cardOffers">
+      <div
+        className="cardOffers"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <div id="divImage">
-          <img className="imgCard" src={imgO} alt="" />
+          <img
+            className="imgCard"
+            src={isHovered ? imgOwhite : imgOblue}
+            alt=""
+          />
         </div>
         <h2 className="hDosCard">{titleO}</h2>
         <h3 className="hTresCard">{descriptionO}</h3>
--- src/Components/QueOfrecemos/QueOfrecemos.tsx
+++ src/Components/QueOfrecemos/QueOfrecemos.tsx
@@ -24,34 +24,40 @@ const QueOfrecemos = () => {
       </div>
       <div className="queOfrecemos">
         <CardOffer
-          imgO={mobile}
+          imgOblue={mobile}
+          imgOwhite={whiteMobile}
           titleO="Diseño Responsivo de Alta Calidad:"
           descriptionO="Nuestros sitios web están diseñados para lucir espectaculares en cualquier dispositivo. Desde smartphones hasta computadoras de escritorio, tu página se verá perfecta en todos ellos."
         />
 
         <CardOffer
-          imgO={blueSeo}
+          imgOblue={blueSeo}
+          imgOwhite={whiteSeo}
           titleO="Experiencia de Usuario Optimizada:"
           descriptionO="Creamos sitios web que no solo se ven bien, sino que también brindan una experiencia de usuario excepcional. La navegación fluida y la disposición intuitiva mantendrán a tus visitantes comprometidos."
         />
         <CardOffer
-          imgO={blueCarga}
+          imgOblue={blueCarga}
+          imgOwhite={whiteCarga}
           titleO="Carga Rápida y Eficiente:"
           descriptionO="Con nuestra tecnología optimizada, garantizamos que tu sitio web cargará de manera rápida y eficiente, manteniendo a tus visitantes involucrados desde el primer momento."
         />
 
         <CardOffer
-          imgO={blueSearch}
+          imgOblue={blueSearch}
+          imgOwhite={whiteSearch}
           titleO="SEO Incorporado:"
           descriptionO="La optimización para motores de búsqueda es crucial para ser encontrado en línea. Integramos técnicas de SEO en cada página que creamos, lo que aumentará tus posibilidades de aparecer en los resultados de búsqueda."
         />
         <CardOffer
-          imgO={blueSocial}
+          imgOblue={blueSocial}
+          imgOwhite={whiteSocial}
           titleO="Redes Sociales:"
           descriptionO="Conecta tu presencia en línea de manera más efectiva mediante la integración perfecta de tus perfiles de redes sociales en tu sitio web. Esto te permite llegar a un público más amplio y mantener a tus visitantes actualizados."
         />
         <CardOffer
-          imgO={blueSupport}
+          imgOblue={blueSupport}
+          imgOwhite={whiteSupport}
           titleO="Soporte y Mantenimiento:"
           descriptionO="No te dejamos solo, ofrecemos mantenimiento y soporte constante para asegurarnos de que tu web siga funcionando sin problemas y esté al día con las últimas tendencias."
         />
