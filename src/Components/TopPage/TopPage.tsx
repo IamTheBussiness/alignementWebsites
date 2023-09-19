2f8f3d970b4ef2dd713dcb756c2955789622cb73
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-09-19 18:32:47 +0200

    logo
    Cambio de nombre de logo

--- src/Components/TopPage/TopPage.tsx
+++ src/Components/TopPage/TopPage.tsx
@@ -1,26 +1,30 @@
 import "./TopPage.css";
 import WorkButton from "../WorkButton/WorkButton";
 import CarouselTop from "../Carousel/CarouselTop";
+import logo from "../../Resources/images/LOGO2.svg";
 const TopPage = () => {
-    return (
-        <>
-            <div id="div-top">
-                <div id="logo" >
-                    <img src="./Resources/images/logo.png" alt="Alignment Websites" />
-                </div>
-                <div id="header-text">
-                    <span >OBTEN UN SITIO WEB PROFESIONAL<br /> Y POTENCIA TU NEGOCIO</span>
-                </div>
-                <div id="div-workButton">
-                    <WorkButton text={'Trabaja con nosotros'} />
-                </div>
+  return (
+    <>
+      <div id="div-top">
+        <div id="logo">
+          <img src={logo} alt="Alignment Websites" />
+        </div>
+        <div id="header-text">
+          <span>
+            OBTEN UN SITIO WEB PROFESIONAL
+            <br /> Y POTENCIA TU NEGOCIO
+          </span>
+        </div>
+        <div id="div-workButton">
+          <WorkButton text={"Trabaja con nosotros"} />
+        </div>
 
-                <div id="carousel-header">
-                    <CarouselTop />
-                </div>
-
-            </div>
-        </>)
+        <div id="carousel-header">
+          <CarouselTop />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TopPage;
