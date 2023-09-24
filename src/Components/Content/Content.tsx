04dfb916857228f7ed8c4fddc15f1cf5f465366e
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-09-24 15:31:48 +0200

    deploy error
    quitando errores de deploy

--- src/Components/Content/Content.tsx
+++ src/Components/Content/Content.tsx
@@ -4,7 +4,7 @@ import QueOfrecemos from "../QueOfrecemos/QueOfrecemos";
 import { QuienesSomos } from "../QuienesSomos/QuienesSomos";
 import FourIcons from "../FourIcons/FourIcons";
 import "./Content.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 const Content = () => {
   const [isQueOfrecemosVisible, setIsQueOfrecemosVisible] = useState(false);
@@ -12,57 +12,82 @@ const Content = () => {
   const [isFaqVisible, setIsFaqVisible] = useState(false);
   const [isFooterVisible, setIsFooterVisible] = useState(false);
   const [isFourIcons, setIsFourIcons] = useState(false);
-  const handleScroll = () => {
+
+  const handleScroll = useCallback(() => {
     const scrollTop = window.scrollY;
     const windowHeight = window.innerHeight;
 
     if (!isQueOfrecemosVisible) {
-      const queOfrecemosElement = document.querySelector(".que-ofrecemos");
+      const queOfrecemosElement = document.querySelector(
+        ".que-ofrecemos"
+      ) as HTMLElement | null;
       const queOfrecemosOffset = queOfrecemosElement?.offsetTop;
-      if (scrollTop > queOfrecemosOffset - windowHeight) {
-        setIsQueOfrecemosVisible(true);
+      if (queOfrecemosOffset !== undefined && queOfrecemosOffset !== null) {
+        if (scrollTop > queOfrecemosOffset - windowHeight) {
+          setIsQueOfrecemosVisible(true);
+        }
       }
     }
 
     if (!isFourIcons) {
-      const fourIconsElement = document.querySelector(".four-icons");
+      const fourIconsElement = document.querySelector(
+        ".four-icons"
+      ) as HTMLElement | null;
       const fourIconsOffset = fourIconsElement?.offsetTop;
-      if (scrollTop > fourIconsOffset - windowHeight) {
-        setIsFourIcons(true);
+      if (fourIconsOffset !== undefined && fourIconsOffset !== null) {
+        if (scrollTop > fourIconsOffset - windowHeight) {
+          setIsFourIcons(true);
+        }
       }
     }
 
     if (!isQuienesSomosVisible) {
-      const quienesSomosElement = document.querySelector(".quienes-somos");
+      const quienesSomosElement = document.querySelector(
+        ".quienes-somos"
+      ) as HTMLElement | null;
       const quienesSomosOffset = quienesSomosElement?.offsetTop;
-      if (scrollTop > quienesSomosOffset - windowHeight) {
-        setIsQuienesSomosVisible(true);
+      if (quienesSomosOffset !== undefined && quienesSomosOffset !== null) {
+        if (scrollTop > quienesSomosOffset - windowHeight) {
+          setIsQuienesSomosVisible(true);
+        }
       }
     }
 
     if (!isFaqVisible) {
-      const faqElement = document.querySelector(".faq");
+      const faqElement = document.querySelector(".faq") as HTMLElement | null;
       const faqOffset = faqElement?.offsetTop;
-      if (scrollTop > faqOffset - windowHeight) {
-        setIsFaqVisible(true);
+      if (faqOffset !== undefined && faqOffset !== null) {
+        if (scrollTop > faqOffset - windowHeight) {
+          setIsFaqVisible(true);
+        }
       }
     }
 
     if (!isFooterVisible) {
-      const footerElement = document.querySelector(".footer");
+      const footerElement = document.querySelector(
+        ".footer"
+      ) as HTMLElement | null;
       const footerOffset = footerElement?.offsetTop;
-      if (scrollTop > footerOffset - windowHeight) {
-        setIsFooterVisible(true);
+      if (footerOffset !== undefined && footerOffset !== null) {
+        if (scrollTop > footerOffset - windowHeight) {
+          setIsFooterVisible(true);
+        }
       }
     }
-  };
+  }, [
+    isQueOfrecemosVisible,
+    isFourIcons,
+    isQuienesSomosVisible,
+    isFaqVisible,
+    isFooterVisible,
+  ]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [handleScroll]);
 
   return (
     <>
--- src/Components/QuienesSomos/QuienesSomos.tsx
+++ src/Components/QuienesSomos/QuienesSomos.tsx
@@ -1,51 +1,12 @@
-// import "./QuienesSomos.css";
-// import WorkButton from "../WorkButton/WorkButton";
-// import { useState } from "react";
-
-// export const QuienesSomos = () => {
-//   const [isAnimationActive, setIsAnimationActive] = useState(false);
-
-//   const activateAnimation = () => {
-//     setIsAnimationActive(true);
-//   };
-
-//   return (
-//     <div id="div-QuienesSomos" className="seccion">
-//       <div className="roll-in-left" id="img-QuienesSomos"></div>
-//       <div id="div-QuienesSomosContent">
-//         <div id="title-QuienesSomos">
-//           <h2>Sobre nosotros</h2>
-//         </div>
-//         <div id="text-QuienesSomos">
-//           <h3>
-//             Lorem Ipsum is simply dummy text of the printing and typesetting
-//             industry. Lorem Ipsum has been the industry's standard dummy text
-//             ever since the 1500s, when an unknown printer took a galley of type
-//             and scrambled it to make a type specimen book. Lorem Ipsum has been
-//             the industry's standard dummy text ever since the 1500s, when an
-//             unknown printer took a galley of type and scrambled it to make a
-//             type specimen book.
-//           </h3>
-//         </div>
-//         <div id="workButton-QuienesSomos">
-//           <WorkButton text={"Trabaja con nosotros"} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// codigo con animacion
-
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import "./QuienesSomos.css";
 import WorkButton from "../WorkButton/WorkButton";
 
 export const QuienesSomos = () => {
   const [isAnimationActive, setIsAnimationActive] = useState(false);
 
-  const handleScroll = () => {
-    const quienesSomosElement = document.getElementById("div-QuienesSomos");
+  const handleScroll = useCallback(() => {
+    const quienesSomosElement = document.getElementById("div-QuienesSomos")!;
     const scrollTop = window.scrollY;
     const elementOffset = quienesSomosElement.offsetTop;
     const windowHeight = window.innerHeight;
@@ -53,14 +14,14 @@ export const QuienesSomos = () => {
     if (scrollTop > elementOffset - windowHeight && !isAnimationActive) {
       setIsAnimationActive(true);
     }
-  };
+  }, [isAnimationActive]);
 
   React.useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [handleScroll]);
 
   return (
     <div
