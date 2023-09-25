5038abe0170cf0cc9985634c16527ee8f248a653
Maximiliano Chavez <104655710+MaxiChavez@users.noreply.github.com>
2023-09-25 20:16:32 +0200

    f
    fix faq

--- src/Components/Faq/FaqContent.tsx
+++ src/Components/Faq/FaqContent.tsx
@@ -1,15 +1,11 @@
 import Faq from "./Faq";
+
 import "./faq.css";
 
 const FaqContent = () => {
   return (
     <div>
-      <Faq faqQuestion="Pregunta 1" faqAnswer="Respuesta 1" />
-      <Faq faqQuestion="Pregunta 2" faqAnswer="Respuesta 2" />
-      <Faq faqQuestion="Pregunta 2" faqAnswer="Respuesta 2" />
-      <Faq faqQuestion="Pregunta 2" faqAnswer="Respuesta 2" />
-      <Faq faqQuestion="Pregunta 2" faqAnswer="Respuesta 2" />
-      <Faq faqQuestion="Pregunta 2" faqAnswer="Respuesta 2" />
+      <Faq />
     </div>
   );
 };
